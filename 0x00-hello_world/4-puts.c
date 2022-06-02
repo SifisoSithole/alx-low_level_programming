d02b7e38a3afe22b3b5bf1f6f97fc8ee99b8fe8f
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-02 13:50:33 +0200

    4-puts fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - prints "Programming is like building a multilingual puzzle,
+ * using the puts fucntion
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like bulding a multilingual puzzle\n");
+	puts("\"Programming is like bulding a multilingual puzzle\n");
 	return (0);
 }
