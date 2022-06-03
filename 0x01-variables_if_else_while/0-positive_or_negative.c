ff753cb76efc436b7152f712305002b01f1862c6
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-03 12:15:32 +0200

    Created README.md and 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: This program evaluetes a random number stored in n.
+ * If the number is greater than 0, it prints "is positive". If the number is 0
+ * it prints "is zero". If  the number is less than 0, it prints "is negative"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char message[];
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		message = "is positive";
+
+	if (n == 0)
+		message = "is zero";
+
+	if (n < 0)
+		message = "is negative";
+
+	printf("%ld %s\n", n, message);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+This directory contains the following C projects:
+
+0-positive_or_negative.c - This program evaluetes a random number stored in n. If the number is greater than 0, it prints "is positive". If the number is 0, it prints "is zero". If  the number is less than 0, it prints "is negative"
+
