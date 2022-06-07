9638c296085d735f43a9b26fb401cb86a63b7050
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-07 15:32:05 +0200

    Fix: 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - This function computes the absolute value of an integer
+ *
+ * @n: an integer
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+
+	return (n);
+}
