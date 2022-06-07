7b4e4ff109fd7dae02aa6834a7f0c6b1db497cb1
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-07 23:45:13 +0200

    Victory

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,30 +7,42 @@
  */
 int main(void)
 {
-	int sum = 0, cnt3 = 0, cnt5 = 0;
+	int sum = 0, cnt3 = 0, cnt5 = 0, i, cap = 0;
 
-	while (sum < 1024)
+
+	while (cap < 1024)
 	{
+		sum = sum + cap;
 		if (cnt3 == 5)
 		{
 			cnt3 = 0;
+			cap = cap + 3;
 		}
 		else
 		{
-			sum = sum + 3;
+			cap = cap + 3;
 		}
 		cnt3++;
 
+	}
+
+	cap = 0;
+
+	while (cap < 1024)
+	{
 		if (cnt5 == 3)
 		{
 			cnt5 = 0;
+			cap = cap + 5;
 		}
 		else
 		{
-			sum = sum + 5;
+			sum = sum + cap;
+			cap = cap + 5;
 		}
 		cnt5++;
 	}
+
 	printf("%d\n", sum);
 
 	return (0);
