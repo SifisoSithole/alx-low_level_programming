3f6cf441857027ea7721881895b59f90239b22b7
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-07 18:12:31 +0200

    Fix: 9-times_tables.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,23 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int x, y, num;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			_putchar(y + '0');
+			num = y * x;
+			if (num < 10)
+			{
+				_putchar(num + '0');
+			}
+			else
+			{
+				_putchar((num / 10));
+				_putchar((num % 10));
+			}
+
 			if (y != 9)
 			{
 				_putchar(',');
