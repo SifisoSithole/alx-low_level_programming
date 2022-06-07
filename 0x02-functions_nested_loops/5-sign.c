1b27f1b453b48e87c414ac6abda1e47223f184dc
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-07 15:13:02 +0200

    Fix: 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,19 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		retrnValue = 1;
 	}
 
 	if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		retrnValue = 0;
 	}
 
 	if (n < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		retrnValue = -1;
 	}
 
