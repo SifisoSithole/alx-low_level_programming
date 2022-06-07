41a25caf6ca6c739b2103631b896a8b71b1badd3
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-07 11:26:06 +0200

    Update: README.md and add: 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Description - This program prints the string "_putchar" using
+ * the putchar function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char str[8] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+This directory contains the following C programs:
+
+0-_putchar.c -  Description - This program prints the string "_putchar" using the putchar function
+
+
