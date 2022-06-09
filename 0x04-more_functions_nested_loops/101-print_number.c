72ed1c09ffaa3d276e3252d2be2eab5d09e78785
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-10 01:05:10 +0200

    try

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_numbr - function
+ * @n: parameter
+ */
+void print_numbr(int n)
+{
+	if (n / 10)
+		print_numbr(n);
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_number - Function
+ * @n: argument
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_numbr(n);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -23,4 +23,8 @@ void print_square(int size);
 
 void print_triangle(int size);
 
+void print_number(int n);
+
+void print_numbr(int n);
+
 #endif
