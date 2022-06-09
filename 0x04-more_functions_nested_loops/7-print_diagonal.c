2c763d1d06c056f337ebb933b0f40ae9b10fc7a8
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-09 18:06:29 +0200

    fixing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,12 +17,11 @@ void print_diagonal(int n)
 
 	for (x = 0; x < n; x++)
 	{
-		for (y = 0; y <= x; y++)
+		for (y = 0; y < x; y++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
-		
 }
