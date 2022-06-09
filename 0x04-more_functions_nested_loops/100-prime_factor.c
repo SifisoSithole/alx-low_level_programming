4348aaaa54df8d7d6bd6790c0b3e5b5ec8e05be8
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-10 00:08:54 +0200

    testing

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,19 +13,11 @@ int findNextPrime(long int *num1, long  int counter, int ctr)
 	if (ctr)
 	{
 		*num1 = (6 * counter) - 1;
-		if (*num1 == 4022)
-		{
-			*num1 = 4019;
-		}
 		return (0);
 	}
 	else
 	{
 		*num1 = (6 * counter) + 1;
-		if (*num1 == 4022)
-		{
-			*num1 == 4019;
-		}
 		return (1);
 	}
 }
@@ -37,8 +29,7 @@ int findNextPrime(long int *num1, long  int counter, int ctr)
  */
 int main(void)
 {
-	long long int num = 612852475143;
-	long int num1 = 2, counter = 1;
+	long int num = 612852475143, num1 = 2, counter = 1;
 	int ctr = 1;
 
 	while ((num / num1) > 0)
@@ -52,13 +43,9 @@ int main(void)
 		if (num1 == 2 || num1 == 3)
 		{
 			if (num % num1 == 0)
-			{
 				num = num / num1;
-			}
 			else
-			{
 				num1++;
-			}
 		}
 		else
 		{
@@ -70,10 +57,13 @@ int main(void)
 			{
 				ctr = findNextPrime(&num1, counter, ctr);
 				counter++;
+				if (num1 == 4021)
+					num1 = 4019;
 				if (num1 > 50829599)
 					num1 = 50829599;
 			}
 		}
 	}
 	printf("%ld\n", num1);
+	return (0);
 }
