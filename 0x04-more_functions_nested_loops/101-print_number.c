bc360d50b1229ef16442735ef33861488c4508df
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2022-06-10 02:34:01 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,11 +5,7 @@
  */
 void print_numbr(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	if (n / 10 != 0)
+	if (n / 10)
 		print_numbr(n / 10);
 
 	_putchar((n % 10) + '0');
@@ -25,6 +21,12 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = n * -1;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
 	}
 
 	print_numbr(n);
