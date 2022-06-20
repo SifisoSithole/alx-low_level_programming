3901e028eace077e5098de5f60d2205b9f620335
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-20 16:51:21 +0200

    Task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,15 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sum = 0;
 	
-	for (i = 0; i < size; i++)
-		sum = sum + **(a)[i];
-
+	for (i = 0; i < (size * size); i += (size + 1))
+		sum += *(a + i);
 	printf("%d, ", sum);
+
+	sum = 0;
+	for (i = 0; i < (size * size); i += (size - 1))
+	{
+		if (i != 0 && i != (size * size) - 1)
+			sum += *(a + i);
+	}
+	printf("%d\n", sum);
 }
