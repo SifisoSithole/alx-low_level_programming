41bc3731cea44860cfcffa9860788d036408dfaf
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-20 17:59:45 +0200

    fix

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - This function sets the value of a pointer to a char
+ * @s: Pointer to the string
+ * @to: Pointer to string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
