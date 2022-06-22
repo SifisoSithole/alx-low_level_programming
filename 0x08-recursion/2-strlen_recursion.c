ea39ce9b4f44b500df4328652aacc663329703f9
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-22 11:32:42 +0200

    Add: 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen_recursion - this function returns the lenght of string
+ * @s: String to count
+ *
+ * Return: Lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 1; 
+
+	if (*s == '\0')
+		return (0);
+
+	n = n + _strlen_recursion(s + 1);
+
+	return (n);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,5 @@
 0-puts_recursion - This function prints string
 
 1-print_rev_recursion - This function prints string in reverse
+
+2-strlen_recursion.c - this function returns the lenght of string
