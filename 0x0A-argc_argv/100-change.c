09d479936f13b2d0c07d188d5a0e14406c02207e
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-24 17:00:00 +0200

    Add: 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments passed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents = 0;
+	int sum = 0;
+	int noCoins = 0;
+	int i = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	if (cents < 0)
+		printf("0\n");
+	else
+	{
+		cents = atoi(argv[1]);
+		while (sum != cents)
+		{
+			if ((sum + coins[i]) > cents)
+			{
+				while ((sum + coins[i]) > cents)
+					i += 1;
+			}
+
+			sum += coins[i];
+
+			noCoins++;
+		}
+		printf("%d\n", noCoins);
+	}
+
+	return (1);
+}
+
+			
