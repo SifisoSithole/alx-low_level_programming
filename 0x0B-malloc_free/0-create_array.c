152974c386b177ccbbfed27a97bbc18fd51f5d65
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-27 15:17:49 +0200

    Fix: 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	buffer = malloc(size * sizeof(char));
+	if (buffer == NULL || size == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		buffer[i] = c;
 
