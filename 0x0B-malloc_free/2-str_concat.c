804eb90e93235ca983d524cc2dbedd4d6ff53384
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-27 17:40:44 +0200

    Fix: 2-str_concate.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,21 +31,29 @@ char *str_concat(char *s1, char *s2)
 	unsigned int index;
 	char *buffer;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		lenS1 = 0;
-		lenS2 = _strlen(s2);
-	}
-	else
-		lenS1 = _strlen(s1);
-
-	if (s2 == NULL && s1 != NULL)
-	{
 		lenS2 = 0;
-		lenS1 = _strlen(s1);
 	}
 	else
-		lenS2 = _strlen(s2);
+	{
+		if (s1 == NULL)
+		{
+			lenS1 = 0;
+			lenS2 = _strlen(s2);
+		}
+		else
+			lenS1 = _strlen(s1);
+
+		if (s2 == NULL && s1 != NULL)
+		{
+			lenS2 = 0;
+			lenS1 = _strlen(s1);
+		}
+		else
+			lenS2 = _strlen(s2);
+	}
 
 	buffer = malloc((lenS1 + lenS2 + 1) * sizeof(char));
 	if (buffer == NULL)
