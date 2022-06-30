f1b947e6a67dc6270b240c3de5926cec292b23eb
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2022-06-30 20:04:54 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,8 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *tempArr;
-	int i;
-	
+	unsigned int i;
+
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == NULL)
