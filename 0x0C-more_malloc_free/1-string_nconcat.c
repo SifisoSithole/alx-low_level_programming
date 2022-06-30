be04f3225c9f2affe36a8542aba246cf9c85a5a8
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-30 13:07:59 +0200

    Add

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - This function returns string length
+ * @s: String
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * string_nconcat - This function concatenates two strings
+ * @s1: Base string
+ * @s2: String to add
+ * @n: Number of bytes from s2
+ *
+ * Return: Pointer to the newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int lenS1;
+	unsigned int lenS2;
+	unsigned int i, index;
+	char *conS;
+
+	if  (s1 == NULL)
+		lenS1 = 0;
+	else if (s2 == NULL)
+		lenS2 = 0;
+	else
+	{
+		lenS1 = _strlen(s1);
+		lenS2 = _strlen(s2);
+	}
+	
+	if (n >= lenS2)
+		n = lenS2;
+	
+	conS = malloc(sizeof(char) * (lenS1 + n));
+	if (conS == NULL)
+		return (NULL);
+
+	for (i = 0; i < lenS1; i++)
+		conS[i] = s1[i];
+	
+	index = i;
+	for (i = 0; i < n; i++)
+	{
+		conS[index] = s2[i];
+		index++;
+	}
+	conS[index] = '\0';
+
+	return (conS);
+}
