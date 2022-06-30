679fca4a002b37024f4510b1903101b749f58e58
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-30 20:52:54 +0200

    Fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			tempArr[i] = ((char *)ptr)[i];
 	}
-	
+
 	free(ptr);
 	return (tempArr);
 }
