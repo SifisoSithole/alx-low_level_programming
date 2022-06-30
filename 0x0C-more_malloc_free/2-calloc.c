7c70b607da1348b1aeac2d9e7ceb00fc080a3c9b
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-06-30 15:36:07 +0200

    Fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < d; i++)
 		mem[i] = 0;
 	return (mem);
 }
