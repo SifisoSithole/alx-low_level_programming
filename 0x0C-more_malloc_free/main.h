176b77ffa874dc20f2c3abd444f0fd660eb19f38
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2022-06-30 20:02:28 +0200

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
-#ifndef HEADER_FILE
+8#ifndef HEADER_FILE
 #define HEADER_FILE
 
 void *malloc_checked(unsigned int b);
@@ -8,5 +8,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
