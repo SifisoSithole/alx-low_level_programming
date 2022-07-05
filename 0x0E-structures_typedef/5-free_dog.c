b8a40eb4036fbd37283f9394420b0c5a8424ac97
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-05 15:44:31 +0200

    Add

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - This function prints frees memory
+ * @d: entry structure with pointer
+ * */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,4 +27,6 @@ typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
+void free_dog(dog_T *d);
+
 #endif
