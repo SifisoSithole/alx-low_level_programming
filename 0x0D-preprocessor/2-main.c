7de43831f1a51ffa8d6b12c29a30750793ff96ae
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-05 16:14:16 +0200

    Add

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+ (11 sloc)  187 Bytes
+
+#include <stdio.h>
+
+/**
+ * main - This function prints the name of the file it was compiled from
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
