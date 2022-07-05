8637db76c136ec06e27a322b5bbbe5bd66a965bf
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-05 14:50:34 +0200

    Add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
 #ifndef DOG_H
-#define DOG_H
+#define Dog_H
 
 /**
  * struct dog - Structure containig dogs details
@@ -15,5 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
