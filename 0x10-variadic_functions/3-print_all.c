aa437d4a08f8486c8162709f46fb7716cd28cc01
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-07 18:04:43 +0200

    Add

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,18 +2,6 @@
 #include <stdlib.h>
 #include "variadic_functions.h"
 
-/**
- * print_char - This function prints char data type
- * @c: Character type
- */
-void print_char(char c, ...)
-{
-	va_list chr;
-
-	va_start(chr, c);
-	printf("%c", va_arg(chr, int);
-	va_end(chr);
-}
 
 /**
  * print_int - This function print integers
@@ -24,7 +12,15 @@ void print_int(char c, ...)
 	va_list integer;
 
 	va_start(integer, c);
-	printf("%d", va_arg(integer, int));
+	switch (c)
+	{
+		case 'i':
+			printf("%d", va_arg(integer, int));
+			break;
+		case 'c':
+			printf("%c", va_arg(integer, int));
+			break;
+	}
 	va_end(integer);
 }
 
@@ -37,7 +33,7 @@ void print_float(char c, ...)
 	va_list flot;
 
 	va_start(flot, c);
-	printf("%f", va_arg(flot, float *));
+	printf("%f", va_arg(flot, double));
 	va_end(flot);
 }
 
@@ -48,20 +44,29 @@ void print_float(char c, ...)
 void print_string(char c, ...)
 {
 	va_list string;
+	char *str = va_arg(string, char *);
 
 	va_start(string, c);
-	printf("%s", va_arg(string, char *));
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 	va_end(string);
 }
 
 /**
  * corr_func - This function selects the correct print function
  * @c: character representing the data type
+ *
+ * Return: Pointer to associated function
  */
 void (*corr_func(char c))(char c, ...)
 {
 	type data_type[] = {
-		{'c', print_char},
+		{'c', print_int},
 		{'i', print_int},
 		{'f', print_float},
 		{'s', print_string},
@@ -76,4 +81,49 @@ void (*corr_func(char c))(char c, ...)
 		i++;
 	}
 	return (NULL);
+}
+
+/**
+ * print_all - This function prints everything
+ * @format: String format
+ */
+void print_all(const char * const format, ...)
+{
+	va_list para;
+	int i = 0;
+	void (*print_func)(char c, ...);
+
+	va_start(para, format);
+	while (format[i] != '\0')
+	{
+		print_func = corr_func(format[i]);
+		switch (format[i])
+		{
+			case 'c':
+				print_func(format[i], va_arg(para, int));
+				break;
+			case 'i':
+				print_func(format[i], va_arg(para, int));
+				break;
+			case 'f':
+				print_func(format[i], va_arg(para, double));
+				break;
+			case 's':
+				print_func(format[i], va_arg(para, char *));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+		switch (format[i])
+		{
+			case '\0':
+				putchar('\n');
+				break;
+			default:
+				printf(", ");
+		}
+	}
+	va_end(para);
 }
