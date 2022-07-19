3b0767b869874af3e3243740c4a60e440c58baa0
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-19 11:09:46 +0200

    Add: 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,15 +5,15 @@
  * free_listint2 - This function frees a linked list
  * @head: head of the linked list
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *h;
 
-	while (head)
+	while (*head)
 	{
-		h = head->next;
-		free(head);
-		head = h;
+		h = (**head).next;
+		free(*head);
+		*head = h;
 	}
-	head = NULL;
+	*head = NULL;
 }
