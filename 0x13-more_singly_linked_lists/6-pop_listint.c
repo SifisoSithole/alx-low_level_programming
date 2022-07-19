77e810c0d2f1b41cca43d50f844981649dae7ea4
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-19 12:27:29 +0200

    Add:6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,18 +5,18 @@
  * pop_listint - This function deletes the head of the node
  * @head: Address of the head node
  *
- * Returns: the head nodes data
+ * Return: the head nodes data
  */
 int pop_listint(listint_t **head)
 {
 	int n;
 	listint_t *h;
 
-	if (head == NULL)
+	if (!head || !*head)
 		return (0);
-	n = *head->n;
-	h = *head->next;
+	n = (*head)->n;
+	h = (*head)->next;
 	free(*head);
-	*head = h->next;
+	*head = h;
 	return (n);
 }
