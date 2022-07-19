24552f4ed8fb805d752020e2f43afc5c7a1993d6
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-19 18:01:07 +0200

    Add

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint_safe - print list
+ * @h: head node
+ *
+ * Return: amount of nodes in list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int len = 0;
+	int i;
+	listint_t *head;
+	const listint_t *adr[100000];
+
+	if (!h || !*h)
+		exit(98);
+
+	while (*h)
+	{
+		for (i = 0; i < len; i++)
+		{
+			if (adr[i] == *h)
+			{
+				free(*h);
+				*h = NULL;
+				return (len);
+			}
+		}
+		adr[len] = *h;
+		len++;
+		head = (*h)->next;
+		free(*h);
+		*h = head;
+	}
+	return (len);
+}
