9d10c58adc906428ea4253b307fe7b7c6ac73cf8
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-19 10:43:59 +0200

    add: 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,27 +10,28 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t new;
+	listint_t *new, *h;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	h = *head;
 	if (head != NULL)
 	{
-		while (1);
+		while (1)
 		{
-			if (*head->next == NULL)
+			if (h->next == NULL)
 			{
-				*head->next = &new;
+				h->next = new;
 				break;
 			}
 			else
-				*head++;
+				h = h->next;
 		}
 	}
-	return (&new)
+	return (new);
 }
 
 
