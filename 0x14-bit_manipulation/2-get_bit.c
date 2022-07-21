27fa7588f13d4221f0dd8b48f156c11c69771162
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-21 19:47:19 +0200

    Add

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * get_bit - Returns bit at index
+  * @n: Number
+  * @index: The index to find
+  *
+  * Return: The bit or -1 on failer
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (n)
+	{
+		if (i == index)
+		{
+			if (n % 2)
+				return (1);
+			else
+				return (0);
+		}
+
+		n = n / 2;
+		i++;
+	}
+
+	if (index > i && index < 63)
+		return (0);
+
+	return (-1);
+}
