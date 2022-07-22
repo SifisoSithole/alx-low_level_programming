efed974b387a19ebcbd8682dbfcc5fec3166147a
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-22 12:05:46 +0200

    Add: 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,11 @@
+/**
+ * set_bit - This function sets the bit at index to 1
+ * @n: Number to change
+ * @index: Index of the bit to change
+ *
+ * Return: 1 on sucess or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	return ((*n |= 1 << index) ? 1 : -1);
+}
