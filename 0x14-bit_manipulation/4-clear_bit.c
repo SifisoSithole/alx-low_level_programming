0fb010f19b08383d719a1e03cd28242004d13433
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-07-22 12:26:30 +0200

    Add: 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+/**
+ * clear_bit - This function clears the bit at index to 0
+ * @n: Number to change
+ * @index: Index of the bit to clear
+ *
+ * Return: 1 on sucess or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	return ((*n &= ~(1 << index)) ? 1 : -1);
+}
