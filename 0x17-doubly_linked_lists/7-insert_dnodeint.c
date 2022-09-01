843ee3859afca5aa16d9acdd12c76b1f2fcaf417
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-09-01 14:04:32 +0200

    Add: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
-		return (new)
+		return (new);
 	}
 	if (!h || !*h)
 		return (NULL);
