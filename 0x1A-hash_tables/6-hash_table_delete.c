f269029120a3e5ff335f8a41ac166120c161e497
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-23 11:42:33 +0200

    Add: 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
